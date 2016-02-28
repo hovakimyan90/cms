2d510a7eaa8f1b7892780d56fd4bccd62ad2604e
Garik Harutyunyan <garik.harutyunyan99@gmail.com>
2016-02-28 01:52:35 -0800

    Create getPostById function in Post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $table = 'posts';
+
+    public static function getPostById($id)
+    {
+        $post = Post::find($id);
+        return $post;
+    }
 }
