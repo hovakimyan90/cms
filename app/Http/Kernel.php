5b6622abe4e41c9dd2b6be6753f7259e58df5cc9
Garik Harutyunyan <garik.harutyunyan99@gmail.com>
2016-07-03 07:19:27 -0700

    change

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -12,12 +12,12 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
-        \App\Http\Middleware\Maintenance::class,
         \App\Http\Middleware\EncryptCookies::class,
         \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
         \Illuminate\Session\Middleware\StartSession::class,
         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
         \App\Http\Middleware\VerifyCsrfToken::class,
+        \App\Http\Middleware\Maintenance::class,
     ];
 
     /**
